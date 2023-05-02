b0a52ca74c37d8efc7f5fe3370ef857978703bf4
Luciefifi <niyomutonilucie@gmail.com>
2023-05-02 18:54:55 +0300

    new branch

--- src/App.jsx
+++ src/App.jsx
@@ -21,17 +21,27 @@ function App() {
             <p className="text-slate-500"> you have a new message</p>
           </div>
         </div>
-        </div>
 
-/* <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-<div class="shrink-0">
-  <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo">
+
+        <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+  <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={chatlogo} alt="Woman's Face"/>
+  <div className="text-center space-y-2 sm:text-left">
+    <div className="space-y-0.5">
+      <p className="text-lg text-black font-semibold">
+        Erin Lindford
+      </p>
+      <p className="text-slate-500 font-medium">
+        Product Engineer
+      </p>
+    </div>
+    <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+  </div>
 </div>
-<div>
-  <div class="text-xl font-medium text-black">ChitChat</div>
-  <p class="text-slate-500">You have a new message!</p>
-</div>
-</div> */
+
+        </div>
+        
+
+
        
       
  
